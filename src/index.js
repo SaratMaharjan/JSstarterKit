f14756d4b8c932c4f46725aed37851bd36a6afc3
Sarat Maharjan <Sarat Maharjan>
2017-04-14 14:21:38 +0200

    mm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import './index.css';
 
-import numeral from 'numeral';
+import {getUsers, deleteUser} from './api/userApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
+// import numeral from 'numeral';
+// const courseValue = numeral(1000).format('$0,0.00');
 // debugger;
-console.log(`I would pay ${courseValue}`);
+// console.log(`I would pay ${courseValue}`);
+
+getUsers().then(result => {
+	let userBody = "";
+
+	result.forEach(user => {
+		userBody += ` </tr>
+			<td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+			<td>${user.id}</td>
+			<td>${user.firstName}</td>
+			</tr>
+		`
+	});
+
+	global.document.getElementById('users').innerHTML = userBody;
+
+	const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+	Array.from(deleteLinks, link => {
+		link.onclick = function(event) {
+			const element = event.target;
+			event.preventDefault();
+			deleteUser(element.attributes["data-id"].value);
+			const row = element.parentNode.parentNode;
+			row.parentNode.removeChild(row);
+		};
+	});
+
+});
