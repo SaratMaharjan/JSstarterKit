2fff272425934930826cb9c6751b755a5e3232d2
saratmaharjan <sarat.maharjan@gmail.com>
2017-06-11 19:43:27 +0200

    minor clean up

--- src/index.test.js
+++ src/index.test.js
@@ -9,14 +9,14 @@ describe('first test', () => {
 	});
 });
 
-/* describe('index.html', () => {
-	it('should have h1 with users', (done) => {
-		const index = fs.readFileSync('./src/index.html', 'utf-8');
-		jsdom.env(index, (err, window) => {
-			const h1 = window.document.getElementsByTagName('h1')[0];
-			expect(h1.innerHTML).to.equal("Users");
-			done();
-			window.close();
-		});
-	});
-}); */
+// describe('index.html', () => {
+// 	it('should have h1 with users', (done) => {
+// 		const index = fs.readFileSync('./src/index.html', 'utf-8');
+// 		jsdom.env(index, (err, window) => {
+// 			const h1 = window.document.getElementsByTagName('h1')[0];
+// 			expect(h1.innerHTML).to.equal('Users');
+// 			done();
+// 			window.close();
+// 		});
+// 	});
+// });
