176f3fec173381f41c6ff0c8d1b7129252d7e8e2
saratmaharjan <sarat.maharjan@gmail.com>
2017-06-14 17:41:23 +0200

    daemonite

--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -12,3 +12,6 @@ import './bootstrap-custom';
 // import 'bootstrap-material-design/dist/js/material.min.js';
 // import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
 // $.material.init();
+
+// import 'daemonite-material/css/material.css';
+// import 'daemonite-material/js/material';
